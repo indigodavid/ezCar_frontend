dc4c0b3f0f9934cabcfbd9d9366521a6e3340571
BregornOriginal <juliog_14@hotmail.com>
2022-10-20 16:20:03 -0300

    Create route for the add form page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Splash from './pages/Splash';
+import AddCardForm from './pages/add_car/AddCardForm';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/add/car" element={<AddCardForm />} />
       </Routes>
     </div>
   );
---
+++ src/pages/add_car/AddCardForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddCardForm() {
+  return (
+    <div>
+      <h1>This is a add form</h1>
+    </div>
+  );
+}
+
+export default AddCardForm;
