f33205bc9d611f4f70485ea1d73cfdb6bffc393e
BregornOriginal <juliog_14@hotmail.com>
2022-10-24 18:02:33 -0300

    Added login component on the splash screen

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LogIn from './LogIn/LogIn';
 
 function Splash() {
   return (
     <div>
       <h1>This is a Splash page</h1>
+      <LogIn />
     </div>
   );
 }
