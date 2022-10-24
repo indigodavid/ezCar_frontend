9ad824980f7cbfceea1127e273dc6f78d64a9917
darikmohammed <r.m.darik@gmail.com>
2022-10-24 23:30:10 +0300

    Add button toggler for reservation

---
+++ src/components/ReserveCar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ReserveCar(props) {
+  const { ReserveOpener } = props;
+  return (
+    <div>
+      <h1>Reserve car form</h1>
+    </div>
+  );
+}
+
+export default ReserveCar;
