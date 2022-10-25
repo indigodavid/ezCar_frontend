4a512e9364db3b92c6176fc7b589b5af5f3882d0
BregornOriginal <juliog_14@hotmail.com>
2022-10-25 20:02:53 -0300

    Added a case to mantain the session when the user refresh the page

--- src/data-api/logInUser.jsx
+++ src/data-api/logInUser.jsx
@@ -1,7 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BASE_URL, LOG_IN } from './routes';
 
-const logInUser = createAsyncThunk('LOG_IN', async (userinfo) => {
+const logInUser = createAsyncThunk('LOG_IN', async (userinfo = {}) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const response = await fetch(`${BASE_URL + LOG_IN}`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        accept: 'application/json',
+        Authorization: token,
+      },
+    });
+    localStorage.setItem('token', response.headers.get('Authorization'));
+    const user = await response.json();
+    return user;
+  }
   const response = await fetch(`${BASE_URL + LOG_IN}`, {
     method: 'POST',
     headers: {
