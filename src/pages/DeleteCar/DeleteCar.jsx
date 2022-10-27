7f081f9eb8dabba32431a9941df839dc8ad8ccd0
BregornOriginal <juliog_14@hotmail.com>
2022-10-27 14:05:08 -0300

    Added Components to list on delete page with a delete button

--- src/pages/DeleteCar/DeleteCar.jsx
+++ src/pages/DeleteCar/DeleteCar.jsx
@@ -3,25 +3,63 @@ import CarCard from '../../components/CarCard';
 
 export default function DeleteCar() {
   return (
-    <div>
-      <CarCard
-        img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
-        name="Hyundai Venue"
-        carType="Kia Sonet"
-        carBrand="Hyundai"
-        carPrice="40000"
-        carColor="Red"
-      />
-      <button
-        type="submit"
-        className="self-end py-2 px-2 bg-lime-500 rounded-full my-4"
-      >
-        Delete
-      </button>
-    </div>
+    <section className="pb-4">
+      <div className="flex flex-col items-center justify-center py-16 md:pb-32">
+        <h1 className=" text-4xl font-bold text-center">Available Cars.</h1>
+        <p className=" text-xs text-slate-400">Lists of all cars for delete</p>
+      </div>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <CarCard
+          id={1}
+          img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
+          name="Hyundai Venue"
+          carType="Kia Sonet"
+          carBrand="Hyundai"
+          carPrice="40000"
+          carColor="Red"
+          deleteCar
+        />
+        <CarCard
+          id={2}
+          img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
+          name="Hyundai Venue"
+          carType="Kia Sonet"
+          carBrand="Hyundai"
+          carPrice="40000"
+          carColor="Red"
+          deleteCar
+        />
+        <CarCard
+          id={3}
+          img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
+          name="Hyundai Venue"
+          carType="Kia Sonet"
+          carBrand="Hyundai"
+          carPrice="40000"
+          carColor="Red"
+          deleteCar
+        />
+        <CarCard
+          id={4}
+          img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
+          name="Hyundai Venue"
+          carType="Kia Sonet"
+          carBrand="Hyundai"
+          carPrice="40000"
+          carColor="Red"
+          deleteCar
+        />
+        <CarCard
+          id={5}
+          img="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75"
+          name="Hyundai Venue"
+          carType="Kia Sonet"
+          carBrand="Hyundai"
+          carPrice="40000"
+          carColor="Red"
+          deleteCar
+        />
+      </div>
+    </section>
   );
 }
-
-DeleteCar.defaultProps = {
-
-};
