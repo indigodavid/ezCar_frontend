588aa619585855bdc3ac224819b65b8b1ee8e4c1
darikmohammed <r.m.darik@gmail.com>
2022-10-28 19:38:41 +0300

    Create the slice for reservation

---
+++ src/redux/ReservationSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import getReservations from '../data-api/getReservation';
+
+const initialState = {
+  status: 'idle',
+  reservation: [],
+};
+
+const reservationSlice = createSlice({
+  name: 'reservation',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getReservations.fulfilled, (state, action) => ({
+      ...state,
+      status: 'success',
+      cars: action.payload,
+    }));
+    builder.addCase(getReservations.pending, (state) => ({
+      ...state,
+      status: 'loading',
+    }));
+    builder.addCase(getReservations.rejected, (state) => ({
+      ...state,
+      status: 'failed',
+    }));
+  },
+});
+
+export const reservationsRedux = reservationSlice.actions;
+export default reservationSlice.reducer;
