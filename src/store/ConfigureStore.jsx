76a2c2c741927f5c0b7b336842c9b4ca76d1276a
darikmohammed <r.m.darik@gmail.com>
2022-10-30 20:04:51 +0300

    Minor change in the configStore -- removing unwanted dispatch

--- src/store/ConfigureStore.jsx
+++ src/store/ConfigureStore.jsx
@@ -4,8 +4,6 @@ import tokenLogger from '../data-api/tokenLogger';
 import userSlice from '../redux/UserSlice';
 import carSlice from '../redux/CarSlice';
 import ReservationSlice from '../redux/ReservationSlice';
-import getCars from '../data-api/getCars';
-import getReservations from '../data-api/getReservation';
 
 const rootReducer = combineReducers({
   user: userSlice,
@@ -18,7 +16,5 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 store.dispatch(tokenLogger());
-store.dispatch(getCars());
-store.dispatch(getReservations());
 
 export default store;
